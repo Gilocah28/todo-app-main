a9344c7b3cd94b12f5b3af78c2c9a1e589556026
Gilocah28 <gberxx@gmail.com>
2023-12-21 22:55:46 +0800

    theme done

---
+++ script/app.js
@@ -0,0 +1,31 @@
+const imageSouce = [
+    `<picture>
+    <source media="(max-width:600px)" srcset="./images/bg-mobile-light.jpg">
+    <img src="./images/bg-desktop-light.jpg" alt="background">
+    </picture>`,
+    `<picture>
+    <source media="(max-width:600px)" srcset="./images/bg-mobile-dark.jpg">
+    <img src="./images/bg-desktop-dark.jpg" alt="background">
+    </picture>`
+]
+
+const imageBackground = document.querySelector('.background-One')
+const mainCoainter = document.getElementById('main')
+const moonToggle = document.getElementById('moon')
+
+moonToggle.addEventListener('click', () => {
+    moonToggle.style.display = 'none'
+    sunToggle.style.display = 'block'
+    mainCoainter.classList.toggle('dark-mode')
+
+    imageBackground.innerHTML = imageSouce[1]
+})
+
+const sunToggle = document.getElementById('sun')
+sunToggle.addEventListener('click', () => {
+    sunToggle.style.display = 'none'
+    moonToggle.style.display = 'block'
+    mainCoainter.classList.toggle('dark-mode')
+
+    imageBackground.innerHTML = imageSouce[0]
+})
