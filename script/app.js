c9b6014f9829368330f97168d313fd5257cc25fa
Gilocah28 <gberxx@gmail.com>
2023-12-22 13:42:23 +0800

    done Themes

--- script/app.js
+++ script/app.js
@@ -1,38 +1,67 @@
 const imageSouce = [
-    `<picture>
-    <source media="(max-width:600px)" srcset="./images/bg-mobile-light.jpg">
-    <img src="./images/bg-desktop-light.jpg" alt="background">
-    </picture>`,
-    `<picture>
-    <source media="(max-width:600px)" srcset="./images/bg-mobile-dark.jpg">
-    <img src="./images/bg-desktop-dark.jpg" alt="background">
-    </picture>`
+    {
+        source: `<picture>
+        <source media="(max-width:600px)" srcset="./images/bg-mobile-light.jpg">
+        <img src="./images/bg-desktop-light.jpg" alt="background">
+        </picture>`,
+        className: '',
+        moonDisplay: 'block',
+        sunDisplay: 'none'
+    },
+    {
+        source: `<picture>
+        <source media="(max-width:600px)" srcset="./images/bg-mobile-dark.jpg">
+        <img src="./images/bg-desktop-dark.jpg" alt="background">
+        </picture>`,
+        className: 'dark-mode',
+        moonDisplay: 'none',
+        sunDisplay: 'block'
+    }
 ]
 
-const themeSource = JSON.parse(localStorage.getItem('theme')) || []
 
 const imageBackground = document.querySelector('.background-One')
 const mainCoainter = document.getElementById('main')
 const moonToggle = document.getElementById('moon')
+const toggle = document.querySelector('.toggle-mode')
+const sunToggle = document.getElementById('sun')
 
 
-const loader = (theme) => {
-    
+const themeSource = JSON.parse(localStorage.getItem('theme')) || []
+
+
+window.addEventListener('load', (e) => {
+    e.preventDefault()
+    getItem(themeSource)
+})
+
+function getItem(item) {
+    const { source, className, moonDisplay, sunDisplay } = item
+    moonToggle.style.display = moonDisplay
+    sunToggle.style.display = sunDisplay
+    imageBackground.innerHTML = source
+    mainCoainter.className = className
 }
 
 moonToggle.addEventListener('click', () => {
-    moonToggle.style.display = 'none'
-    sunToggle.style.display = 'block'
-    mainCoainter.classList.toggle('dark-mode')
+    localStorage.setItem('theme', JSON.stringify(imageSouce[1]));
+    getItem(themeSource)
+    const { source, className, moonDisplay, sunDisplay } = imageSouce[1]
+    moonToggle.style.display = moonDisplay
+    sunToggle.style.display = sunDisplay
+    imageBackground.innerHTML = source
+    mainCoainter.className = className
+});
 
-    imageBackground.innerHTML = imageSouce[1]
-})
-
-const sunToggle = document.getElementById('sun')
 sunToggle.addEventListener('click', () => {
-    sunToggle.style.display = 'none'
-    moonToggle.style.display = 'block'
-    mainCoainter.classList.toggle('dark-mode')
+    localStorage.setItem('theme', JSON.stringify(imageSouce[0]));
+    getItem(themeSource)
+    const { source, className, moonDisplay, sunDisplay } = imageSouce[0]
+    moonToggle.style.display = moonDisplay
+    sunToggle.style.display = sunDisplay
+    imageBackground.innerHTML = source
+    mainCoainter.className = className
+});
 
-    imageBackground.innerHTML = imageSouce[0]
-})
+
+
